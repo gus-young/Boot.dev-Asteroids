a66f58f7731605bb3b389af845ef1756e59813fb
Gus Young <76542590+gus-young@users.noreply.github.com>
2026-05-30 20:03:47 -0400

    Added player

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
+#Screen Config
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+#Player Config
+PLAYER_RADIUS = 20 
+LINE_WIDTH = 2
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import player
 
 def main():
     pygame.init()
@@ -9,6 +10,8 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    user_player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,9 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        user_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
